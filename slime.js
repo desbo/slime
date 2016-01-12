519aa094d3b2fe86ce081de05f596e47f28f53db
Sam Desborough <sam.desborough@gmail.com>
2016-01-12 10:38:29 +0000

    initial

---
+++ slime.js
@@ -0,0 +1,71 @@
+function reduce(xs, f, acc) {
+  switch (xs.constructor) {
+    case Array:
+      return reduceArray(xs, f, acc);
+
+    case Object:
+      return reduceObject(xs, f, acc);
+
+    default:
+      throw new TypeError('nah');
+      break;
+  }
+}
+
+function reduceArray(xs, f, acc) {
+  for (var i = 0; i < xs.length; i++) {
+    acc = f(acc, xs[i], i);
+  }
+
+  return acc;
+}
+
+function reduceObject(obj, f, acc) {
+  var keys = Object.keys(obj);
+  
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    acc = f(acc, obj[key], key);
+  }
+
+  return acc;
+}
+
+function map(xs, f) {
+  return reduce(xs, function(acc, x, i) {
+    return acc.concat(f(x, i));
+  }, []);
+}
+
+function filter(xs, f) {
+  return reduce(xs, function(acc, x, i) {
+    return f(x, i) ? acc.concat(x) : acc;
+  }, []);
+}
+
+function some(xs, f) {
+  return reduce(xs, function(acc, x, i) {
+    return f(x, i) ? true : acc;
+  }, false);
+}
+
+function every(xs, f) {
+  return reduce(xs, function(acc, x, i) {
+    return f(x, i) ? acc : false;
+  }, true);
+}
+
+function forEach(xs, f) {
+  for (var i = 0; i < xs.length; i++) {
+    f(xs[i], i);
+  }
+}
+
+module.exports = {
+  reduce: reduce,
+  map: map,
+  filter: filter,
+  some: some,
+  every: every,
+  forEach: forEach
+}
